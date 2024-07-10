5f493d0ab980cf303d5ec733435d1145053050a9
Amit Dharmandra Chaurasiya <Amit.Chaurasiya@asl.mum.sst>
2024-07-10 17:37:25 +0530

    Files copy pasted from local directory

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber, factorial = 1, i;
+            Console.Write("Enter a number : ");
+            inputNumber = int.Parse(Console.ReadLine());
+
+            for (i = 1; i <= inputNumber; i++)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine("Factorial of " + inputNumber + " is " + factorial);
+        }
+    }
+}
